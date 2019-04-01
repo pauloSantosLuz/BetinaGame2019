95f161d5f64f2d6061d2ae3877dc372d2bed8bdd
Paulo <paulohenrique.luz@hotmail.com>
2019-04-01 10:43:14 -0300

    Projeto gameBetina 2019

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Transform bulletSpawn;
+	public GameObject bulletPrefab;
+
+    void Update()
+    {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;        
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0,x,0);
+		transform.Translate(0,0,z);
+
+		if(Input.GetKeyDown(KeyCode.Space))
+			Fire();
+    }
+
+    void Fire()
+    {
+    	var bullet = (GameObject) Instantiate(bulletPrefab,
+    											bulletSpawn.position,
+    											bulletSpawn.rotation);
+    	bullet.GetComponent<Rigidbody>().velocity = 
+    						bullet.transform.forward * 6;
+    }
+}
